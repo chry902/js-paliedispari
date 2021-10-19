42ccee76bc7d7fee583b259709339c0aae32b0c0
chry902 <tchry902@gmail.com>
2021-10-19 17:42:22 +0200

    esercizio 1, inserimento dei valori sul html

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 
 
-let stringa = revString("radaR");
+let stringaIniziale = prompt(revString(""));
 
 function revString(str) {
     let splString = str.split("");
     //controlliamo se la stringa e stata inserita correttamente 
-    console.log(splString);
+    //console.log(splString);
 
     let revArray = splString.reverse();
     //controlliamo se la stringa è stata invertita
-    console.log(revArray);
+    //console.log(revArray);
 
     return revArray.join("");
 
 }
 
 //controlliamo se la stringa e stata riassemblata
-console.log(`${stringa}`);
+
+
+let stringaFinale = revString(stringaIniziale);
+//console.log(`${stringaFinale}`);
+
+if (stringaFinale && stringaIniziale) {
+    document.querySelector(".text").innerHTML = `La parola inserita è ${stringaIniziale} ed è stata cambiabiata correttamente in ${stringaFinale}`;
+} else stringaFinale || stringaIniziale; {
+    document.querySelector(".text").innerHTML = `Spiacente la parola inserita è ${stringaIniziale} , voltandola non combaciano ${stringaFinale}`;
+}
