6f53e07cfda628ff6b95d36436d0cd736d95cf3a
chry902 <tchry902@gmail.com>
2021-10-19 16:00:08 +0200

    ultimo consol.log

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ var stringa = revString("radaR");
 
 function revString(str) {
     var splString = str.split("");
-
-    console.log();
+    //controlliamo se la stringa e stata inserita correttamente 
+    console.log(splString);
 
     var revArray = splString.reverse();
-
+    //controlliamo se la stringa è stata invertita
+    console.log(revArray);
 
     return revArray.join("");
 
 }
+
+//controlliamo se la stringa e stata riassemblata
+console.log("revArray");
