31bff0de889e662a92a2caaa17f9383dd1eac3fc
chry902 <tchry902@gmail.com>
2021-10-19 15:51:34 +0200

    palidroma

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+var stringa = revString("radaR");
+
+function revString(str) {
+    var splString = str.split("");
+
+    console.log();
+
+    var revArray = splString.reverse();
+
+
+    return revArray.join("");
+
+}
